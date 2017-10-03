7ce2852f483ad9059617ddc0784897fa71224d1e
Kevin Piacentini <kevinpiac@gmail.com>
2017-10-03 08:56:41 +0200

    change router

--- src/router/index.js
+++ src/router/index.js
@@ -6,14 +6,35 @@ import guard from './guard';
 Vue.use(Router);
 
 const router = new Router({
+  /**
+   * Remove the default and ugly hash mode ('#' in url)
+   *
+   * https://router.vuejs.org/en/essentials/history-mode.html
+   */
   mode: 'history',
+
+  /**
+   * Inject our routes object
+   */
   routes,
+
+  /**
+   * Scroll top left on route change
+   *
+   * https://router.vuejs.org/en/advanced/scroll-behavior.html
+   */
+  scrollBehavior: () => ({ x: 0, y: 0 }),
+
 });
 
 /**
  * Apply a guard method before each route to check if it requires auth
  */
 router.beforeEach((to, from, next) => {
+  /**
+   * If the route meta 'auth' key is set to true we consider
+   * this route as a protected one
+   */
   if (to.meta.auth === true) {
     guard.guardRoute(to, from, next);
   } else {
